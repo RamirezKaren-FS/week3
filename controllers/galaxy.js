32e8bcdb38206022dac101951e91a8736063e4a8
RamirezKaren-FS <kjramirez1@student.fullsail.edu>
2024-07-24 12:09:11 -0500

    updates to galaxy able to show list of galaxies and edit them not able to create a new galaxy

--- controllers/galaxy.js
+++ controllers/galaxy.js
@@ -3,7 +3,17 @@ const { Galaxy, Star } = require("../models/index.js")
 // Show all resources
 const index = async (req, res) => {
   const galaxies = await Galaxy.findAll()
-  res.status(200).json(galaxies)
+  res.status(200).render('Galaxy/index.twig', { galaxies })
+}
+
+const form = async (req, res) => {
+  const { id } = req.params
+  if (typeof id !== "undefined")
+  {
+    const galaxy = await Galaxy.findByPk( req.params.id )
+    return res.status(200).render(`Galaxy/edit.twig`, { galaxy })
+  } 
+    res.render('Galaxy/new.twig')
 }
 
 // Show resourc
@@ -12,26 +22,26 @@ const show = async (req, res) => {
     include: [ Star ]
   } )
   // const stars = await galaxy.getStars()
-  res.status(200).json(galaxy)
+  res.status(200).render('Galaxy/show.twig', { galaxy })
 }
 
 // Create a new resource
 const create = async (req, res) => {
   const galaxy = await Galaxy.create(req.body)
-  res.status(201).json(galaxy)
+  res.status(302).redirect('/galaxies')
 }
 
 // Update an existing resource
 const update = async (req, res) => {
   const galaxy= await Galaxy.update(req.body, { where: {id: req.params.id}} )
-  res.status(200).json(galaxy)
+  res.status(302).redirect(`/galaxies/${req.params.id}`)
 }
 
 // Remove a single resource
 const remove = async (req, res) => {
   Galaxy.destroy( { where: { id: req.params.id} } )
-  res.status(204).json(true)
+  res.status(302).redirect('/galaxies')
 }
 
 // Export all controller actions
-module.exports = { index, show, create, update, remove }
+module.exports = { index, show, create, update, remove, form }
